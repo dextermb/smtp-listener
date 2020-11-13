3b91519e38387af3949684b399c93a0cdade23da
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2020-11-13 19:56:16 +0000

    Add support for SSL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const SMTP = require('smtp-server').SMTPServer
 const hook = require('./hook')
 
-const server = new SMTP({
+const opts = {
   name: process.env.HOSTNAME,
   onAuth: hook.authenticate,
   onConnect: hook.connect,
@@ -9,8 +9,20 @@ const server = new SMTP({
   onData: hook.data,
   onMailFrom: hook.from,
   onRcptTo: hook.to
-})
+}
 
+const key = process.env.KEY_PATH
+const cert = process.env.CERT_PATH
+
+if (key && cert) {
+  const fs = require('fs')
+
+  opts.secure = true
+  opts.key = fs.readFileSync(key)
+  opts.cert = fs.readFileSync(cert)
+}
+
+const server = new SMTP(opts)
 const port = process.env.PORT
 
 server.listen(port, () => {
